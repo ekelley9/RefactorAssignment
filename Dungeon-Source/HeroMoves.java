62dba7e0b866e738046d6802b8be1c159777ff2c
ekelley9 <57920465+ekelley9@users.noreply.github.com>
2019-11-20 15:23:37 -0800

    made dungeon character parameters private and changed hero constructors

---
+++ Dungeon-Source/HeroMoves.java
@@ -0,0 +1,6 @@
+
+public interface HeroMoves {
+	
+	public int move();
+
+}
--- Dungeon-Source/Warrior.java
+++ Dungeon-Source/Warrior.java
@@ -15,10 +15,10 @@
 public class Warrior extends Hero
 {
 
-    public Warrior()
+    public Warrior(String name)
 	{
 
-		super("Warrior", 125, 4, .8, 35, 60, .2);
+		super(name, 125, 4, .8, 35, 60, .2);
 
 
     }//end constructor
@@ -29,13 +29,13 @@ public class Warrior extends Hero
 		if (Math.random() <= .4)
 		{
 			int blowPoints = (int)(Math.random() * 76) + 100;
-			System.out.println(name + " lands a CRUSHING BLOW for " + blowPoints
+			System.out.println(getName() + " lands a CRUSHING BLOW for " + blowPoints
 								+ " damage!");
 			opponent.subtractHitPoints(blowPoints);
 		}//end blow succeeded
 		else
 		{
-			System.out.println(name + " failed to land a crushing blow");
+			System.out.println(getName() + " failed to land a crushing blow");
 			System.out.println();
 		}//blow failed
 
@@ -43,7 +43,7 @@ public class Warrior extends Hero
 
 	public void attack(DungeonCharacter opponent)
 	{
-		System.out.println(name + " swings a mighty sword at " +
+		System.out.println(getName() + " swings a mighty sword at " +
 							opponent.getName() + ":");
 		super.attack(opponent);
 	}//end override of attack method
