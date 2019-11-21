9d4a675526aae723a979ce1fc91d5c075c0e2fd1
Garrett Lawless <garrett@blahs-MacBook-Pro.local>
2019-11-21 13:22:53 -0800

    got rid of the keyboard class and made everything a scanner

--- Dungeon-Source/Dungeon.java
+++ Dungeon-Source/Dungeon.java
@@ -1,170 +1,131 @@
-/**
- * Title: Dungeon.java
- *
- * Description: Driver file for Heroes and Monsters project
- *
- * Copyright:    Copyright (c) 2001
- * Company: Code Dogs Inc.
- * I.M. Knurdy
- *
- * History:
- *  11/4/2001: Wrote program
- *    --created DungeonCharacter class
- *    --created Hero class
- *    --created Monster class
- *    --had Hero battle Monster
- *    --fixed attack quirks (dead monster can no longer attack)
- *    --made Hero and Monster abstract
- *    --created Warrior
- *    --created Ogre
- *    --made Warrior and Ogre battle
- *    --added battleChoices to Hero
- *    --added special skill to Warrior
- *    --made Warrior and Ogre battle
- *    --created Sorceress
- *    --created Thief
- *    --created Skeleton
- *    --created Gremlin
- *    --added game play features to Dungeon.java (this file)
- *  11/27/2001: Finished documenting program
- * version 1.0
- */
+import java.util.Scanner;
 
-
-
-/*
-  This class is the driver file for the Heroes and Monsters project.  It will
-  do the following:
-
-  1.  Allow the user to choose a hero
-  2.  Randomly select a monster
-  3.  Allow the hero to battle the monster
-
-  Once a battle concludes, the user has the option of repeating the above
-
-*/
-public class Dungeon
-{
-    public static void main(String[] args)
-	{
+public class Dungeon {
+	public static void main(String[] args) {
 
 		Hero theHero;
 		Monster theMonster;
 
-		do
-		{
-		    theHero = chooseHero();
-		    theMonster = generateMonster();
+		do {
+			theHero = chooseHero();
+			theMonster = generateMonster();
 			battle(theHero, theMonster);
 
 		} while (playAgain());
 
-    }//end main method
+	}// end main method
 
-/*-------------------------------------------------------------------
-chooseHero allows the user to select a hero, creates that hero, and
-returns it.  It utilizes a polymorphic reference (Hero) to accomplish
-this task
----------------------------------------------------------------------*/
-	public static Hero chooseHero()
-	{
-		int choice;
+	public static Hero chooseHero() {
+		Scanner kb = new Scanner(System.in);
+		int choice = 0;
 		Hero theHero;
 
-		System.out.println("Choose a hero:\n" +
-					       "1. Warrior\n" +
-						   "2. Sorceress\n" +
-						   "3. Thief");
-		choice = Keyboard.readInt();
+		System.out.println("Choose a hero:\n" + "1. Warrior\n" + "2. Sorceress\n" + "3. Thief");
+		choice = kb.nextInt();
 
-		switch(choice)
-		{
-			case 1: return new Warrior();
+		switch (choice) {
+		case 1:
+			return characterCreator(1);
 
-			case 2: return new Sorceress();
+		case 2:
+			return characterCreator(2);
 
-			case 3: return new Thief();
+		case 3:
+			return characterCreator(3);
 
-			default: System.out.println("invalid choice, returning Thief");
-				     return new Thief();
-		}//end switch
-	}//end chooseHero method
+		default:
+			System.out.println("invalid choice, returning Thief");
+			return characterCreator(0);
+		}// end switch
+	}// end chooseHero method
 
-/*-------------------------------------------------------------------
-generateMonster randomly selects a Monster and returns it.  It utilizes
-a polymorphic reference (Monster) to accomplish this task.
----------------------------------------------------------------------*/
-	public static Monster generateMonster()
-	{
+	/*-------------------------------------------------------------------
+	generateMonster randomly selects a Monster and returns it.  It utilizes
+	a polymorphic reference (Monster) to accomplish this task.
+	---------------------------------------------------------------------*/
+	public static Monster generateMonster() {
 		int choice;
 
-		choice = (int)(Math.random() * 3) + 1;
+		choice = (int) (Math.random() * 3) + 1;
 
-		switch(choice)
-		{
-			case 1: return new Ogre();
+		switch (choice) {
+		case 1:
+			return new Ogre();
 
-			case 2: return new Gremlin();
+		case 2:
+			return new Gremlin();
 
-			case 3: return new Skeleton();
+		case 3:
+			return new Skeleton();
 
-			default: System.out.println("invalid choice, returning Skeleton");
-				     return new Skeleton();
-		}//end switch
-	}//end generateMonster method
+		default:
+			System.out.println("invalid choice, returning Skeleton");
+			return new Skeleton();
+		}// end switch
+	}// end generateMonster method
 
-/*-------------------------------------------------------------------
-playAgain allows gets choice from user to play another game.  It returns
-true if the user chooses to continue, false otherwise.
----------------------------------------------------------------------*/
-	public static boolean playAgain()
-	{
-		char again;
+	/*-------------------------------------------------------------------
+	playAgain allows gets choice from user to play another game.  It returns
+	true if the user chooses to continue, false otherwise.
+	---------------------------------------------------------------------*/
+	public static boolean playAgain() {
+		Scanner kb = new Scanner(System.in);
+		String again;
 
 		System.out.println("Play again (y/n)?");
-		again = Keyboard.readChar();
+		again = kb.next();
 
-		return (again == 'Y' || again == 'y');
-	}//end playAgain method
+		return (again.equals('Y') || again.equals('y'));
 
+	}// end playAgain method
 
-/*-------------------------------------------------------------------
-battle is the actual combat portion of the game.  It requires a Hero
-and a Monster to be passed in.  Battle occurs in rounds.  The Hero
-goes first, then the Monster.  At the conclusion of each round, the
-user has the option of quitting.
----------------------------------------------------------------------*/
-	public static void battle(Hero theHero, Monster theMonster)
-	{
-		char pause = 'p';
-		System.out.println(theHero.getName() + " battles " +
-							theMonster.getName());
+	/*-------------------------------------------------------------------
+	battle is the actual combat portion of the game.  It requires a Hero
+	and a Monster to be passed in.  Battle occurs in rounds.  The Hero
+	goes first, then the Monster.  At the conclusion of each round, the
+	user has the option of quitting.
+	---------------------------------------------------------------------*/
+	public static void battle(Hero theHero, Monster theMonster) {
+		System.out.println(theHero.getName() + " battles " + theMonster.getName());
 		System.out.println("---------------------------------------------");
 
-		//do battle
-		while (theHero.isAlive() && theMonster.isAlive() && pause != 'q')
-		{
-		    //hero goes first
+		// do battle
+		while (theHero.isAlive() && theMonster.isAlive()) {
+			// hero goes first
 			theHero.battleChoices(theMonster);
 
-			//monster's turn (provided it's still alive!)
+			// monster's turn (provided it's still alive!)
 			if (theMonster.isAlive())
-			    theMonster.attack(theHero);
+				theMonster.attack(theHero);
 
-			//let the player bail out if desired
+			// let the player bail out if desired
 			System.out.print("\n-->q to quit, anything else to continue: ");
-			pause = Keyboard.readChar();
 
-		}//end battle loop
+		} // end battle loop
 
 		if (!theMonster.isAlive())
-		    System.out.println(theHero.getName() + " was victorious!");
+			System.out.println(theHero.getName() + " was victorious!");
 		else if (!theHero.isAlive())
 			System.out.println(theHero.getName() + " was defeated :-(");
-		else//both are alive so user quit the game
+		else// both are alive so user quit the game
 			System.out.println("Quitters never win ;-)");
 
-	}//end battle method
+	}// end battle method
 
+	public static Hero characterCreator(int choice) {
+		Scanner kb = new Scanner(System.in);
+		if (choice == 1) {
+			System.out.print("Enter character name: ");
+			return new Warrior();
+		} else if (choice == 2) {
+			System.out.print("Enter character name: ");
+			return new Sorceress();
+		} else if (choice == 3) {
+			System.out.print("Enter character name: ");
+			return new Thief();
+		} else {
+			return new Thief();
+		}
 
-}//end Dungeon class
+	}
+}// end Dungeon class
--- Dungeon-Source/Thief.java
+++ Dungeon-Source/Thief.java
@@ -1,75 +1,53 @@
+import java.util.Scanner;
 
+public class Thief extends Hero {
 
-/**
- * Title:
- * Description:
- * Copyright:    Copyright (c) 2001
- * Company:
- * @author
- * @version 1.0
- */
-
-public class Thief extends Hero
-{
-
-    public Thief()
-	{
+	public Thief() {
 		super("Thief", 75, 6, .8, 20, 40, .5);
 
+	}// end constructor
 
-
-    }//end constructor
-
-	public void surpriseAttack(DungeonCharacter opponent)
-	{
+	public void surpriseAttack(DungeonCharacter opponent) {
 		double surprise = Math.random();
-		if (surprise <= .4)
-		{
-			System.out.println("Surprise attack was successful!\n" +
-								name + " gets an additional turn.");
+		if (surprise <= .4) {
+			System.out.println("Surprise attack was successful!\n" + name + " gets an additional turn.");
 			numTurns++;
 			attack(opponent);
-		}//end surprise
-		else if (surprise >= .9)
-		{
-			System.out.println("Uh oh! " + opponent.getName() + " saw you and" +
-								" blocked your attack!");
-		}
-		else
-		    attack(opponent);
+		} // end surprise
+		else if (surprise >= .9) {
+			System.out.println("Uh oh! " + opponent.getName() + " saw you and" + " blocked your attack!");
+		} else
+			attack(opponent);
 
+	}// end surpriseAttack method
 
-	}//end surpriseAttack method
-
-
-    public void battleChoices(DungeonCharacter opponent)
-	{
+	public void battleChoices(DungeonCharacter opponent) {
+		Scanner kb = new Scanner(System.in);
 		super.battleChoices(opponent);
 		int choice;
 
+		do {
+			System.out.println("1. Attack Opponent");
+			System.out.println("2. Surprise Attack");
+			System.out.print("Choose an option: ");
+			choice = kb.nextInt();
 
-		do
-		{
-		    System.out.println("1. Attack Opponent");
-		    System.out.println("2. Surprise Attack");
-		    System.out.print("Choose an option: ");
-		    choice = Keyboard.readInt();
-
-		    switch (choice)
-		    {
-			    case 1: attack(opponent);
-			        break;
-			    case 2: surpriseAttack(opponent);
-			        break;
-			    default:
-			        System.out.println("invalid choice!");
-		    }//end switch
+			switch (choice) {
+			case 1:
+				attack(opponent);
+				break;
+			case 2:
+				surpriseAttack(opponent);
+				break;
+			default:
+				System.out.println("invalid choice!");
+			}// end switch
 
 			numTurns--;
 			if (numTurns > 0)
-			    System.out.println("Number of turns remaining is: " + numTurns);
+				System.out.println("Number of turns remaining is: " + numTurns);
 
-		} while(numTurns > 0);
+		} while (numTurns > 0);
 
-    }
+	}
 }
